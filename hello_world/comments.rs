1e99538a0466c4082ee07fb1d0b6056a619ec559
Shankara <shankar.narayanan0@hotmail.com>
2025-01-14 21:29:34 -0800

    Have started learning rust. Added simple example hello world codes

---
+++ hello_world/comments.rs
@@ -0,0 +1,14 @@
+// Testing out different types of comments
+
+
+fn main(){
+
+    // Example 1
+    /* Example 2
+     */
+
+    let x = 5 + /* 90 + */ 5;
+    println!("Testing value of x (is it 100 or 10)\n x = {}", x);
+
+
+}
---
+++ hello_world/hello.rs
@@ -0,0 +1,18 @@
+// First code!
+
+fn main() {
+
+    // println! - prints with a newline like "hello\n"
+    println!("Back to hello world after so many years! Welcome");
+
+    // print!() - prints without a newline
+    print!("Testing print!()");
+
+    // String interpolation
+    println!();
+    println!("Name {}", "Marley");
+
+    // Positional arguments
+    println!("{1} {0}", "0", "1");
+
+}
